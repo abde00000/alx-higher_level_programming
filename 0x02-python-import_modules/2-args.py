f945dfb9bd76d7f2b71f7e1729dd1e4485ae724c
abdessamad <abdessamadelmahboub1999@gmail.com>
2023-12-06 01:49:51 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 len = len(sys.argv)
 if len == 1:
-    print(f"{len - 1} arguments.")
+    print(f"{len - 1} argument.")
 else:
     print(f"{len - 1} arguments:")
     for n in range(1, len):
