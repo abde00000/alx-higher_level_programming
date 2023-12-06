e486e380bd5cf4ed988c163d8ecc37940a58162d
abdessamad <abdessamadelmahboub1999@gmail.com>
2023-12-06 01:47:43 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+len = len(sys.argv)
+if len == 1:
+    print(f"{len - 1} arguments.")
+else:
+    print(f"{len - 1} arguments:")
+    for n in range(1, len):
+        argv = sys.argv[n]
+        print("{}: {}".format(n, argv))
