e145d31564baa17c48eeec6727e795ff7a67f4ef
abdessamad <abdessamadelmahboub1999@gmail.com>
2023-12-06 02:10:05 +0100

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        results = 0
-    else:
-        results = 0
-        for n in range(1, len(sys.argv)):
-            results += int(sys.argv[n])
-    print("{}".format(results))
+
+    result = 0
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
