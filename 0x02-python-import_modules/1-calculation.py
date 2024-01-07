b1182759a32f473855591c8eb40df9b40ddc4ca9
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-01-07 14:10:16 +0100

    2nd

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a, b = 10, 5
+    print("{} + {} = {}".format(a, b, str(add(a, b))))
+    print("{} - {} = {}".format(a, b, str(sub(a, b))))
+    print("{} * {} = {}".format(a, b, str(mul(a, b))))
+    print("{} / {} = {}".format(a, b, str(div(a, b))))
