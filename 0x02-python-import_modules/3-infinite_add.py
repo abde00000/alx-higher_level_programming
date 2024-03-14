ba8804ea3d66ee4b61b358b098930b86bb93d416
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-14 23:31:30 +0000

    add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__== "__main__":
+    import sys
+    def add(args):
+        result = 0
+        for i in args:
+            result += int(i)
+        return result
+    print(add(sys.argv[1:]))
