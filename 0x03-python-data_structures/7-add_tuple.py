71a8560b2a7e452703e32a3621314c3944b7ac91
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-16 16:16:22 +0000

    tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2:
+        a, b = tuple_a[:2]
+    elif len(tuple_a) == 1:
+        a, b = tuple_a[0], 0
+    else:
+        a, b = (0, 0)
+    if len(tuple_b) >= 2:
+        c, d = tuple_b[:2]
+    elif len(tuple_b) == 1:
+        c, d = tuple_b[0], 0
+    else:
+        c, d = (0, 0)
+    return a+c, b+d
