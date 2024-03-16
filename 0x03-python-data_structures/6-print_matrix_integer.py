1bb5da2aee53e334a15ce84b6cdb2228673e02db
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-16 14:39:28 +0000

    tasks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        matrix = []
     for list_1 in matrix:
         for num in list_1:
             print("{:d}".format(num), end=" ")
