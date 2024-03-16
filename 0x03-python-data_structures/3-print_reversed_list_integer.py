02345a992eb8e234dccd2e75c3ae408a2b7f7ac7
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-16 14:32:57 +0000

    tasks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=None):
+    if my_list is None:
+        my_list = []
     new_list = my_list.copy()
     new_list.reverse()
     for i in new_list:
