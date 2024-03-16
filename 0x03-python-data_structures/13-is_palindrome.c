7ba979c2ba2b372223dfc86ce97f72b6c74a6c55
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-16 23:14:54 +0000

    task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,14 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-listint_t *reversed(listint_t *head)
+int is_palindrome(listint_t **head)
 {
-    listint_t *current = head, *prev = NULL, *next;
+    listint_t *slow = *head, *fast = *head;
+    listint_t *prev_slow = NULL, *mid = NULL;
+    int is_pal = 1;
 
-    if (current == NULL || current->next == NULL)
+    if (*head == NULL || (*head)->next == NULL)
+        return 1; // Empty list or single node list is palindrome
+
+    // Use slow and fast pointers to find the middle of the list
+    while (fast != NULL && fast->next != NULL)
     {
-        return (current);
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
     }
+
+    if (fast != NULL)
+    {
+        // The list has odd number of nodes, skip the middle node
+        mid = slow;
+        slow = slow->next;
+    }
+
+    // Reverse the first half of the list
+    listint_t *prev = NULL, *next = NULL, *current = *head;
+    while (current != slow)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    // Compare the reversed first half with the second half of the list
+    listint_t *first_half = prev;
+    listint_t *second_half = slow;
+    while (first_half != NULL && second_half != NULL)
+    {
+        if (first_half->n != second_half->n)
+        {
+            is_pal = 0;
+            break;
+        }
+        first_half = first_half->next;
+        second_half = second_half->next;
+    }
+
+    // Restore the original list by reversing the first half again
+    prev = NULL;
+    current = *head;
     while (current != NULL)
     {
         next = current->next;
@@ -17,52 +60,11 @@ listint_t *reversed(listint_t *head)
         prev = current;
         current = next;
     }
-    head = prev;
-    return (head);
-}
+    *head = prev;
 
-void push(listint_t **head, int data)
-{
-    listint_t *new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = data;
-    new_node->next = *head;
-    *head = new_node;
-}
+    // If the list had an odd number of nodes, re-link the middle node
+    if (mid != NULL)
+        prev_slow->next = mid;
 
-int is_palindrome(listint_t **head)
-{
-    listint_t *current = *head;
-    listint_t *tmp = NULL; // Declaration of tmp pointer
-    listint_t *rev;
-    int i, len = 0;
-
-    if (current == NULL)
-        return (0);
-    else if (current->next == NULL)
-        return (1);
-    tmp = NULL;
-    while (current != NULL)
-    {
-        push(&tmp, current->n);
-        len++;
-        current = current->next;
-    }
-    rev = reversed(tmp);
-
-    // Reset current to the head of the list for further use
-    current = *head;
-    for (i = 0; i < len / 2; i++)
-    {
-        if (current->n != rev->n)
-            return (0);
-        current = current->next;
-        rev = rev->next;
-    }
-    
-    return (1);
+    return is_pal;
 }
