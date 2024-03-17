e5e96868d235719befc4cbd3c1740fb97cbd30a9
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-17 22:38:58 +0000

    tasks

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     if my_string == "":
         return my_string
-    no_c_list = []
+    no_c_str = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            no_c_list.append(char)
-    return ''.join(no_c_list)
+            no_c_str += char
+    return no_c_str
