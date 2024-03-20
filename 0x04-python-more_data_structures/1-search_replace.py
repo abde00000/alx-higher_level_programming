9469dfc6f5eb4e805f182669ee64c5992609f51f
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-20 20:59:57 +0000

    4th project tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    index_list = my_list.index(search)
-    new_list[index_list] = replace
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] = replace
     return new_list
