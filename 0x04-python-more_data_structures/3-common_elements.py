a285f58701fcbbbc057dffd0e672170d0858fc18
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-20 21:09:51 +0000

    4th project tasks

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,4 +2,6 @@
 def common_elements(set_1=None, set_2=None):
     if set_1 is None or set_2 is None:
         return None
+    if not isinstance(set_1, set) or not isinstance(set_2, set):
+        return None
     return set_1.intersection(set_2)
