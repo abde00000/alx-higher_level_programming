732ae7f275b93cdd8ca1e994b521e6bc4e3a81be
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-20 20:47:30 +0000

    4th project tasks

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    sq = lambda x: x ** 2
+    for row in matrix:
+        new_matrix.append(list(map(sq, row)))
+    return new_matrix
+          
