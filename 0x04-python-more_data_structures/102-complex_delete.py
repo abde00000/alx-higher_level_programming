95fe374e76dd202f8c3e72742e498104fdc101ea
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-23 17:02:00 +0000

    4th project tasks

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
+    else:
+        del_keys = {key for key, val in a_dictionary.items() if val == value}
+        for key in del_keys:
+                del a_dictionary[key]
+        return a_dictionary
