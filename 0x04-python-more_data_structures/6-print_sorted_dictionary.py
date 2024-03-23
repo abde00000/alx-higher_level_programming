c09bde6247c0a37628810c7d0a50c1e6ce9ecd6d
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-23 15:35:21 +0000

    4th project tasks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     copy_dictionary = a_dictionary.copy()
     sor = sorted(copy_dictionary)
     for key in sor:
-        print("{}: {}".format(key, a_dictionary[key]))
+        print("{}: {}".format(key, copy_dictionary[key]))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,3 +3,4 @@ def update_dictionary(a_dictionary, key, value):
     if type(key) is not str:
         key = str(key)
     a_dictionary[key] = value
+    return a_dictionary
