2947a1d4c140722084bbf77d790563c7e0d4cc21
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-04-05 23:47:53 +0000

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Create a square Class and define it with the attribute size.
+
+Attributes:
+    __size: the size of the square
+Methodes:
+    __init__(size): Initialize size attribute with the value size.
+"""
+class square:
+    """
+        initialize a new square object.
+
+        Args:
+            size: the size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
