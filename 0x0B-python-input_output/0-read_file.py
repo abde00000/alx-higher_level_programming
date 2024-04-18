8750368a884d1965ec0f4867380edcdbcde2844e
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-04-18 01:35:30 +0100

    new task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,7 @@
 
 
 def read_file(filename=""):
-    """ reads a text file (UTF8) and prints it to stdout."""
+    """Reads a text file (UTF8) and prints it to stdout."""
 
     with open(filename, 'r') as file:
         file_content = file.read()
