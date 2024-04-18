ebfb70ccf0e74baf47391109e06d8111a8312610
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-04-18 01:27:18 +0100

    new project

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,7 @@
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout."""
 
-    with open('UTF8', 'r') as file:
+    with open('filename', 'r') as file:
         file_content = file.read()
 
     print(file_content)
