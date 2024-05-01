31959d615e68866428ca973fccbe564a6b700d79
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-05-01 17:06:08 +0100

    updates

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,24 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        self.integer_validation("size", self.width)
+        self.integer_validation("size", value)
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.width = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+
+        if args:
+            self.__update(*args)
+        if kwargs:
+            self.__update(**kwargs)
