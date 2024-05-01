db742cf6f9f7ba2c4a60142e446482f8bdd4b5db
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-05-01 02:56:29 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,32 +17,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        self.integer_validation("width", value, False)
         self.__width = value
 
     @property
     def height(self):
+        """height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        self.integer_validation("height", value, False)
         self.__height = value
 
     @property
     def x(self):
+        """x of the rectangle"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        self.integer_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
+        """y of the rectangle"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        self.integer_validation("y", value)
         self.__y = value
+
+    def integer_validation(self, name, value, eq=True):
+        """ validation of all setter methods and instantiation"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if not eq and value <= 0:
+            raise ValueError(f"{name} must be > 0")
+        if eq and value < 0:
+            raise ValueError(f"{name} must be >= 0")
