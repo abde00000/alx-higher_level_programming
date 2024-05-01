16afd177be18f2d57b59b67d1c393e11f50b4038
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-05-01 03:29:26 +0100

    updates

--- 0x0C-python-almost_a_circle/2-main.py
+++ 0x0C-python-almost_a_circle/2-main.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
         print("[{}] {}".format(e.__class__.__name__, e))
 
     try:
-        Rectangle(10, 2, 3, -1)
+        Rectangle(10, 2, -3, -1)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
