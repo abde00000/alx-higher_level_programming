a8880d0e497e10ef26a23059df5b3621548f7427
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-05-02 00:45:02 +0100

    changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,17 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            n1 = Rectangle(1, 1)
+        elif cls is Square:
+            n1 = Square(1)
+        else:
+            n1 = None
+        n1.update(**dictionary)
+        return n1
